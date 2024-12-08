b6f64364dce60fd29a60ecbb892941ff97e01bd1
Huaxiao Liang <1184903633@qq.com>
2024-12-08 10:14:08 +0800

    move project_test to anothre reporsitory-from-gcc-to-cpp

--- project_test/FunN.cpp
+++
@@ -1,9 +0,0 @@
-/*************************************************************************
-  > File Name: Fun.cpp
-  > Author: Huaxiao Liang
-  > Mail: 1184903633@qq.com
-  > Created Time: Sun 06 Oct 2024 02:50:32 PM CST
- ************************************************************************/
-
-#include "Header.h"
-void ttPrint() { std::cout << "tTest" << std::endl; }
